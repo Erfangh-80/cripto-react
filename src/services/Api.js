69e22974c1bef2925767d6c87e5c413eda3435be
Erfan Gholami <Erfangh4871gholami@gmail.com>
2022-09-04 10:05:11 +0430

    change per page to 50

--- src/services/Api.js
+++ src/services/Api.js
@@ -3,7 +3,7 @@ import axios from "axios"
 const BASE_URL = "https://api.coingecko.com/api/v3/coins/markets"
 
 const getCrypto = async (currency, pageNumber) => {
-    const response = await axios.get(`${BASE_URL}?vs_currency=${currency}&order=market_cap_desc&per_page=30&page=${pageNumber}&sparkline=false`);
+    const response = await axios.get(`${BASE_URL}?vs_currency=${currency}&order=market_cap_desc&per_page=50&page=${pageNumber}&sparkline=false`);
     return response.data
 }
 
