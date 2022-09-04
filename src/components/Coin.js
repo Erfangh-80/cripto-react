40089fc927776e9a8fa4d43fcb312505f49d1795
Erfan Gholami <Erfangh4871gholami@gmail.com>
2022-09-04 10:09:09 +0430

    handle price and price change and added rank

--- src/components/Coin.js
+++ src/components/Coin.js
@@ -5,17 +5,18 @@ import styles from "../assets/styles/Coin.module.css";
 // context
 import { CryptoContext } from '../context/CryptoContextProvider';
 
-const Coin = ({name, image, symbolName, price, marketCap, priceChange}) => {
+const Coin = ({name, rank, image, symbolName, price, marketCap, priceChange}) => {
     
     const {symbol} = useContext(CryptoContext)
 
     return (
         <div className={styles.container}>
+            <span>{rank}</span>
             <img className={styles.image} src={image} alt={name} />
             <span className={styles.name} >{name}</span>
             <span className={styles.symbol} >{symbolName.toUpperCase()}</span>
-            <span className={styles.currentPrice} >{symbol} {price.toLocaleString()}</span>
-            <span className={priceChange > 0 ? styles.greenPriceChange : styles.redPriceChange} >{priceChange.toFixed(2)}</span>
+            <span className={styles.currentPrice} >{symbol} {price ? price.toLocaleString() : "0"}</span>
+            <span className={priceChange > 0 ? styles.greenPriceChange : styles.redPriceChange} >{priceChange ? priceChange.toFixed(2) : "0"}</span>
             <span className={styles.marketCap} >{symbol} {marketCap.toLocaleString()}</span>
         </div>
     );
