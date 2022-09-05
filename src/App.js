7a8899578a36e59714d1790e414a8f64ffd53da2
Erfan Gholami <Erfangh4871gholami@gmail.com>
2022-09-05 19:14:29 +0430

    added routs and spa to project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
 
 // styles
 import "./App.css";
-
 // components
 import Landing from "./components/Landing";
 import Navbar from "./components/template/Navbar";
+import CoinPage from "./components/CoinPage";
 // context
 import CryptoContextProvider from "./context/CryptoContextProvider";
 
@@ -14,7 +15,11 @@ function App() {
     <CryptoContextProvider>
       <div className="App">
         <Navbar />
-        <Landing />
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/coin/:id" element={<CoinPage />} />
+          <Route path="/*" element={<Navigate to="/" />} />
+        </Routes>
       </div>
     </CryptoContextProvider>
   );
