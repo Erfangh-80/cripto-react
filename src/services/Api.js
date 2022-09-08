a3821fe142701b7ef59108e215bd044423ceb5e4
Erfan Gholami <Erfangh4871gholami@gmail.com>
2022-09-08 20:32:04 +0430

    add api getCoin width id to project

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,10 +1,22 @@
-import axios from "axios"
+import axios from "axios";
 
-const BASE_URL = "https://api.coingecko.com/api/v3/coins/markets"
+const BASE_URL = "https://api.coingecko.com/api/v3/coins";
 
 const getCrypto = async (currency, pageNumber) => {
-    const response = await axios.get(`${BASE_URL}?vs_currency=${currency}&order=market_cap_desc&per_page=50&page=${pageNumber}&sparkline=false`);
-    return response.data
-}
+  const response = await axios.get(
+    `${BASE_URL}/markets?vs_currency=${currency}&order=market_cap_desc&per_page=50&page=${pageNumber}&sparkline=false`
+  );
+  return response.data;
+};
 
-export {getCrypto}
+const getChartCoin = async (id, currency, days = 365) => {
+  const response = await axios.get(`${BASE_URL}/${id}/market_chart?vs_currency=${currency}&days=${days}`);
+  return response.data;
+};
+
+const getCoin = async (id) => {
+  const response = await axios.get(`${BASE_URL}/${id}`);
+  return response.data;
+};
+
+export { getCrypto, getCoin, getChartCoin };
